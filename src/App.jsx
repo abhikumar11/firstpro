ee41a799d6b59f35a6a12f5272a4fb8b278d1b85
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-05 13:16:23 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import About from "./components/About";
 
 const App = () => {
   let name="Akshay"
      return (
+      <div>
      <h1>Welcome {name}</h1>
+     <About/>
+     </div>
     );
 };
 
