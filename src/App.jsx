209b3364a9cecaa40646ef0f891b4c62340dd77f
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-08 10:08:11 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Contact from "./components/Contact";
 import Service from "./components/Service";
 import "./App.css";
 import Image from "./components/Image";
+import Hero from "./components/Hero";
 const App = () => {
      return (
           <div>
                <h1 style={{ backgroundColor: "red", color: "white",padding:"20px" }}>
                     Welcome to React
                </h1>
-               <About />
-               <Contact />
+               <About/>
+               <Contact/>
                <Image/>
-               <Service />
-               
+               <Service/>
+               <Hero/>
           </div>
      );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
+import myimg from "../assets/user.jpg"; 
 const Hero = () => {
   return (
     <div>
         <div className="container">
                 <div className="introduction">
-                    
+                    <h3>HEY THERE</h3>
+                    <h2>I AM JO BREED</h2>
+                    <h3>CREATIVE ART DIRECTOR & DESIGNER</h3>
                 </div>
                 <div className="imagesection">
-
+                      <img src={myimg} alt=""/>
                 </div>
         </div>
     </div>
